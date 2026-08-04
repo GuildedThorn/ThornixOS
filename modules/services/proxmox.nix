bb8495f32cd1d69c87c0d4053368dad6584bb513
GuildedThorn <admin@guildedthorn.com>
2026-08-04 00:05:43 -0500

    fix(proxmox): add cdrkit for cloud-init

--- modules/services/proxmox.nix
+++ modules/services/proxmox.nix
@@ -4,10 +4,15 @@
     {
       lib,
       config,
+      pkgs,
       ...
     }:
 
     {
+
+      # The `qm` binary can resolve to the pve-ha-manager wrapper, which does
+      # not include cdrkit in PATH when generating cloud-init images.
+      environment.systemPackages = [ pkgs.cdrkit ];
 
       services.openssh.settings.AcceptEnv = lib.mkForce [
         "LANG"
