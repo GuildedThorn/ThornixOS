8bdd0e467ca2b0fba6a5bc024f07cc07a9f272e2
GuildedThorn <admin@guildedthorn.com>
2026-09-08 08:01:00 -0500

    fix(thornbot): allow workstation through ssh (dummy me)

--- hosts/thornbot/networking.nix
+++ hosts/thornbot/networking.nix
@@ -18,5 +18,16 @@
       "172.16.25.2"
       "172.16.25.1"
     ];
+
+    firewall.extraCommands = ''
+      iptables -w -A nixos-fw -p tcp --dport 22 -s 172.16.25.3/32 -j nixos-fw-accept
+      iptables -w -A nixos-fw -p tcp --dport 22 -s 192.168.1.6/32 -j nixos-fw-accept
+      iptables -w -A nixos-fw -p tcp --dport 22 -s 10.10.10.4/32 -j nixos-fw-accept
+      iptables -w -A nixos-fw -p tcp --dport 1935 -s 192.168.1.6/32 -j nixos-fw-accept
+      iptables -w -A nixos-fw -p tcp --dport 1935 -s 192.168.1.74/32 -j nixos-fw-accept
+      iptables -w -A nixos-fw -p tcp --dport 1935 -s 10.10.10.4/32 -j nixos-fw-accept
+      iptables -w -A nixos-fw -p tcp --dport 8090 -s 172.16.25.51/32 -j nixos-fw-accept
+      iptables -w -A nixos-fw -p tcp --dport 8090 -s 172.16.25.63/32 -j nixos-fw-accept
+    '';
   };
 }
