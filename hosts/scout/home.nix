bb2eeeee0b2335cb68d18dac19b660898139ca9b
GuildedThorn <admin@guildedthorn.com>
2026-09-20 14:25:02 -0500

    feat(sout): add zellij to scout

--- hosts/scout/home.nix
+++ hosts/scout/home.nix
@@ -16,6 +16,7 @@
       thorn.programs.firefox.glanceHomepage = true;
       thorn.programs.ghostty.enable = true;
       thorn.programs.zathura.enable = true;
+      thorn.programs.zellij.enable = true;
       thorn.programs.obsidian.enable = true;
       thorn.programs.claude-code.enable = true;
       thorn.programs.opencode.enable = true;
